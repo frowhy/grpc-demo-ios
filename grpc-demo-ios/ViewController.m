c778546cd3bf2d28db0dcc6b57c150243eb9a6df
Govern Fu <frowhy@gmail.com>
2019-07-02 14:29:43 +0800

    :sparkles: Adding GRPC call test

--- grpc-demo-ios/ViewController.m
+++ grpc-demo-ios/ViewController.m
@@ -7,6 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "GRPCCall+Tests.h"
+#import <Protobuf/GPBMessage.h>
+#import <grpc_demo_ios/grpc-demo-ios-umbrella.h>
+
+static NSString *const kHostAddress = @"localhost:50051";
 
 
 @interface ViewController ()
@@ -17,7 +22,28 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    UITextField *responseMessage = [[UITextField alloc] init];
+    responseMessage.frame = CGRectMake(0, 0, [[UIScreen mainScreen] bounds].size.width, [[UIScreen mainScreen] bounds].size.height);
+    responseMessage.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
+    responseMessage.contentHorizontalAlignment = UIControlContentHorizontalAlignmentCenter;
+    responseMessage.textAlignment = NSTextAlignmentCenter;
+    responseMessage.enabled = NO;
+    responseMessage.text = @"Loading";
+
+    [GRPCCall useInsecureConnectionsForHost:kHostAddress];
+
+    TestAServiceGreeter *client = [[TestAServiceGreeter alloc] initWithHost:kHostAddress];
+    TestAServiceEchoRequest *request = [[TestAServiceEchoRequest alloc] init];
+    request.message = @"iOS";
+
+    [client echoWithRequest:request handler:^(TestAServiceEchoResponse *_Nullable response, NSError *_Nullable error) {
+        if (!error) {
+            [responseMessage setText:response.meta.message];
+        }
+    }];
+
+    [self.view addSubview:responseMessage];
 }
 
 
